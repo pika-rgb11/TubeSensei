e89e67c290ab127074babb1df3db8c4f673224e6
Fariyad Khan <pikachubhaimere@gmail.com>
2026-09-10 12:27:01 +0530

    Enable static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+output: "export",  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
